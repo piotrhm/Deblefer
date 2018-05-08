9da4880afa92d1657db18acc0b7143a5d368d81e
Unknown <helm.piotr@gmail.com>
2018-05-08 16:59:53 +0200

    Custom Dialog update

--- App/app/src/main/java/com/example/deblefer/Classes/CustomDialog.java
+++ App/app/src/main/java/com/example/deblefer/Classes/CustomDialog.java
@@ -6,9 +6,16 @@ import android.content.DialogInterface;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class CustomDialog {
+
+    public interface onGetCardDialogFinish extends Runnable{
+        void addCards(Collection<Card> cards);
+    }
+
     static int count = 0;
     Activity activity;
 
@@ -20,14 +27,17 @@ public class CustomDialog {
         this.activity = activity;
     }
 
-    public AlertDialog showDialog(int limit) {
+    public AlertDialog showDialog(int cardsLimit, Collection<Card> toUse, onGetCardDialogFinish onFinishHandler) {
         // Where we track the selected items
         boolean[] clickedCardTmp = new boolean[52];
 
-        List<Card> Items = Deck.getModifableDeckAsList();
+        List<Card> Items = new ArrayList<>();
+        Items.addAll(toUse);
+        Collections.sort(Items);
         List<String> Tmp = new ArrayList<>();
-        for(int i = 0; i < Items.size(); i++)
+        for(int i = 0; i < Items.size(); i++) {
             Tmp.add(Items.get(i).toString());
+        }
         currentDeck = Tmp.toArray(new String[Tmp.size()]);
 
 
@@ -43,7 +53,7 @@ public class CustomDialog {
                                 count += isMarked ? 1 : -1;
                                 clickedCardTmp[which] = isMarked;
 
-                                if (count > limit) {
+                                if (count > cardsLimit) {
                                     Toast.makeText(activity, "You selected too many.", Toast.LENGTH_SHORT).show();
                                     clickedCardTmp[which] = false;
                                     count--;
@@ -60,6 +70,8 @@ public class CustomDialog {
                         // or return them to the component that opened the dialog
                         clickedCard = clickedCardTmp;
                         count = 0;
+                        onFinishHandler.addCards(getSelectedCard());
+                        onFinishHandler.run();
                     }
                 })
                 .setNegativeButton("CANCEL", new DialogInterface.OnClickListener() {
@@ -72,7 +84,23 @@ public class CustomDialog {
         return builder.create();
     }
 
-    public void getSelectedCard() {
+    public Collection<Card> getSelectedCard() {
+        Collection<Card> toReturn = new ArrayList<>();
 
+        List<String>toFind = new ArrayList<>();
+        for(int i = 0; i < clickedCard.length; i++) {
+            if(clickedCard[i])
+                toFind.add(currentDeck[i]);
+        }
+
+        List<Card> Items = Deck.getModifableDeckAsList();
+        for(int i = 0; i < toFind.size(); i++) {
+            for(int j = 0; j < Items.size(); j++) {
+                if (Items.get(j).toString().equals(toFind.get(i)))
+                    toReturn.add(Items.get(j));
+            }
+        }
+
+        return toReturn;
     }
 }
--- App/app/src/main/java/com/example/deblefer/TexasModuleActivity.java
+++ App/app/src/main/java/com/example/deblefer/TexasModuleActivity.java
@@ -7,8 +7,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
 import android.widget.ImageView;
 
 import com.example.deblefer.Classes.Card;
@@ -30,6 +28,39 @@ public class TexasModuleActivity extends AppCompatActivity {
     private List<ImageView> cardImages = new ArrayList<>();
     private FloatingActionButton addButton;
 
+    class onDialogFinishHandler implements CustomDialog.onGetCardDialogFinish{
+
+        Collection<Card> addedCards = null;
+        @Override
+        public void addCards(Collection<Card> addedCards){
+            this.addedCards = addedCards;
+        }
+
+        @Override
+        public void run() {
+            Card card = addedCards.iterator().next();
+            if (card == null)
+                return;
+            TexasModuleActivity.this.setViewActive(cardImages.get(TexasModuleActivity.this.getUsedCardCount()), card);
+            deck.remove(card);
+            if (TexasModuleActivity.this.getUsedCardCount() < 2)
+                hand.add(card);
+            else
+                table.add(card);
+            if (TexasModuleActivity.this.getUsedCardCount() == 2) {
+                // PREFLOP
+            } else if (TexasModuleActivity.this.getUsedCardCount() == 5) {
+                // FLOP
+            } else if (TexasModuleActivity.this.getUsedCardCount() == 6) {
+                // TURN
+            }
+            if (TexasModuleActivity.this.getUsedCardCount() == 7) {
+                // RIVER
+                addButton.setClickable(false);
+            }
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,28 +75,12 @@ public class TexasModuleActivity extends AppCompatActivity {
         cardImages.add((ImageView)findViewById(R.id.cardImageView6));
         addButton = findViewById(R.id.addCardButton);
 
-        addButton.setOnClickListener((View v) -> {
-            Card card = getCardFromDialog();
-            if(card == null)
-                return;
-            setViewActive(cardImages.get(getUsedCardCount()), card);
-            deck.remove(card);
-            if(getUsedCardCount() < 2)
-                hand.add(card);
-            else
-                table.add(card);
-            if(getUsedCardCount() == 2){
-                // PREFLOP
-            }
-            else if(getUsedCardCount() == 5){
-                // FLOP
-            }
-            else if(getUsedCardCount() == 6){
-                // TURN
-            }
-            if(getUsedCardCount() == 7){
-                // RIVER
-                addButton.setClickable(false);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                CustomDialog dialog = new CustomDialog(TexasModuleActivity.this);
+                AlertDialog alertDialog = dialog.showDialog(1, deck, new onDialogFinishHandler());
+                alertDialog.show();
             }
         });
     }
@@ -93,10 +108,6 @@ public class TexasModuleActivity extends AppCompatActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
-
-    private Card getCardFromDialog(){
-        return new Card(Card.Rank.DEUCE, Card.Suit.HEARTS);
     }
 
     private void setViewUnused(ImageView cardView){
